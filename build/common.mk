2b02ec8fce81d6fb98743239bced507f4cca742d
yhougardy <yhougardy@24c45a59-3718-0410-950f-d1d872dd8675>
2006-11-21 21:16:16 +0000

    Update #1600310 More fixes ...
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1058 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ build/common.mk
@@ -0,0 +1,29 @@
+all: do-all
+
+CC=gcc
+LD=ld
+CSC=gmcs
+CSC_DEBUG=-d:DEBUG -debug+
+RECURSIVE_TARGETS = check clean install uninstall
+
+$(RECURSIVE_TARGETS): %: do-%
+
+do-% : %-recursive
+	$(MAKE) $*-local
+
+%-recursive:
+	@list='$(SUBDIRS)'; for dir in $$list ; do \
+	    (cd $$dir && $(MAKE) $*) || { error_terminate="exit 1"; } ; \
+	done; \
+	$$error_terminate
+
+dist-recursive: dist-local
+	list='$(SUBDIRS)'; for dir in $$list ; do \
+	    (cd $$dir && $(MAKE) distdir=$(distdir)/$$dir $@) || exit 1 ; \
+	done
+
+dist-common:
+	-mkdir -p $(distdir)
+	for file in Makefile $(DISTFILES) ; do \
+	    install -D $$file $(top_srcdir)/$(distdir)/$$file || exit 1 ; \
+	done
