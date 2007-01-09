ba1f99434ad2461db3a5ee41e06a5271217b93f2
yhougardy <yhougardy@24c45a59-3718-0410-950f-d1d872dd8675>
2007-01-09 11:21:35 +0000

    Patch - unified ODF Word add-in (Alain's contrib)
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1384 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/OdfAddInForWordSetup/PostBuild.bat
@@ -0,0 +1,7 @@
+%1AddCustomActions.vbs %2 ..\..\OdfInstallHelper.dll
+%1MakeSetup.exe %1%3
+
+::ECHO."%1">>"c:\documents and settings\yves\mes documents\toto.txt"
+::ECHO."%2">>"c:\documents and settings\yves\mes documents\toto.txt"
+::ECHO."%3">>"c:\documents and settings\yves\mes documents\toto.txt"
+
