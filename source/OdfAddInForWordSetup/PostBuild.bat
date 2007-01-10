f6ab77e0cf14a5021e0d1221246c76ef50c8e939
yhougardy <yhougardy@24c45a59-3718-0410-950f-d1d872dd8675>
2007-01-10 19:33:58 +0000

    Correction
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1403 24c45a59-3718-0410-950f-d1d872dd8675

--- source/OdfAddInForWordSetup/PostBuild.bat
+++ source/OdfAddInForWordSetup/PostBuild.bat
@@ -1,7 +1,6 @@
 %1AddCustomActions.vbs %2 ..\..\OdfInstallHelper.dll
-%1MakeSetup.exe %1%3
+%1MakeSetupExe.bat %1%3
 
-::ECHO."%1">>"c:\documents and settings\yves\mes documents\toto.txt"
-::ECHO."%2">>"c:\documents and settings\yves\mes documents\toto.txt"
-::ECHO."%3">>"c:\documents and settings\yves\mes documents\toto.txt"
+::ECHO.%1AddCustomActions.vbs %2 ..\..\OdfInstallHelper.dll>>"c:\documents and settings\yves\mes documents\toto.txt"
+::ECHO.%1MakeSetupExe.bat %1%3>>"c:\documents and settings\yves\mes documents\toto.txt"
 
