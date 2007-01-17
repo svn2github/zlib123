ef41b3a304e1d998ab786f449bd72b8b46d33c63
yhougardy <yhougardy@24c45a59-3718-0410-950f-d1d872dd8675>
2007-01-17 22:48:45 +0000

    Added polish setup project
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1485 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/OdfAddInForWordSetup-pl/PostBuild.bat
@@ -0,0 +1,6 @@
+%1AddCustomActions.vbs %2 ..\..\OdfInstallHelper.dll
+%1MakeSetupExe.bat %1%3
+
+::ECHO.%1AddCustomActions.vbs %2 ..\..\OdfInstallHelper.dll>>"c:\documents and settings\yves\mes documents\toto.txt"
+::ECHO.%1MakeSetupExe.bat %1%3>>"c:\documents and settings\yves\mes documents\toto.txt"
+
