e62a7ae9d72a30c1c18e94e62acc80f73e85aabb
yhougardy <yhougardy@24c45a59-3718-0410-950f-d1d872dd8675>
2007-02-15 15:21:36 +0000

    Added assembly info on Word Addin project
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1714 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/Word/Add-In/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("WordAddin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clever Age")]
+[assembly: AssemblyProduct("WordAddin.Properties")]
+[assembly: AssemblyCopyright("Copyright © Clever Age 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("b424e8a1-ad08-4c11-b677-f479709cf445")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
