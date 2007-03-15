fdbbb95e171c357a493feaaa23d4ecacf40db4db
venkatesh_son <venkatesh_son@24c45a59-3718-0410-950f-d1d872dd8675>
2007-03-15 13:03:06 +0000

    Presentation Addins
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@1951 24c45a59-3718-0410-950f-d1d872dd8675

--- source/Presentation/Addin/Addin.cs
+++ source/Presentation/Addin/Addin.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using CleverAge.OdfConverter.OdfConverterLib;
+using Sonata.OdfConverter.Presentation;  
 
-namespace Sonata.OdfConverter.Presentation
+namespace Sonata.OdfConverter.Presentaion
 {
     public class Addin : OdfAddinLib
     {
-        public Addin()
-            : base(new Sonata.OdfConverter.Presentation.Converter())
+        public Addin() : base(new Sonata.OdfConverter.Presentation.Converter())
         {
+            this.OverrideResourceManager = new System.Resources.ResourceManager("PresentationAddin.resources.Labels", Assembly.GetExecutingAssembly());
         }
     }
 }
