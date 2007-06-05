5d4d566f245184442e4efb4eebbc02a7a5d91725
venkatesh_son <venkatesh_son@24c45a59-3718-0410-950f-d1d872dd8675>
2007-06-05 19:28:09 +0000

    Added a folder remotely
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@2580 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/Presentation/ODFPPT2003ADDIN/ConverterInterface.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Sonata.OdfConverter.OdfPowerpointAddinLib
+{
+    [ComVisible(true), Guid("11FB3926-34EF-4f0f-8404-399C5285D583"), InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IOdfConverter
+    {
+
+        void OdfToOox(string inputFile, string outputFile, bool showUserInterface);
+        void OoxToOdf(string inputFile, string outputFile, bool showUserInterface);
+    }
+}
