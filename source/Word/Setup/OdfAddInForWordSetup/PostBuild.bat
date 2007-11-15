c2a07e1d301bb5634cdf232ced3998cd03fdf3a4
divo76 <divo76@24c45a59-3718-0410-950f-d1d872dd8675>
2007-11-15 13:30:34 +0000

    Fix for #1789989 Add-in disables "Back" button option in Screen 4
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@4050 24c45a59-3718-0410-950f-d1d872dd8675

--- source/Word/Setup/OdfAddInForWordSetup/PostBuild.bat
+++ source/Word/Setup/OdfAddInForWordSetup/PostBuild.bat
@@ -1,6 +1,9 @@
 %1AddCustomActions.vbs %2 ..\..\..\..\..\lib\OdfInstallHelper.dll
 cscript.exe %1CorrectConditions.vbs %2 %1Conditions.xml
 cscript.exe %1patchForVista.vbs %2
+
+::Bug #1789989  enable back button on wizard page 4
+cscript.exe %1AddProperty.vbs %2
 %1MakeSetupExe.bat %1%3
 
 ::ECHO.%1AddCustomActions.vbs %2 ..\..\..\..\..\lib\OdfInstallHelper.dll>"postBuild.log"
