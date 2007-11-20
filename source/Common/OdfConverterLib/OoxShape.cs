558ae6abdc99b80c18f1244f71b7d9232f0616a2
Satish_son <Satish_son@24c45a59-3718-0410-950f-d1d872dd8675>
2007-11-20 12:10:14 +0000

    Group File
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@4117 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/Common/OdfConverterLib/OoxShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OdfConverter.Transforms;
+
+namespace OdfConverter.Transforms.Test {
+    public class OoxShape {
+        static double Scale = 10.0;
+        static int DeltaX = 0;
+        static int DeltaY = 0;
+
+     
+        OoxTransform _transform;
+
+        public OoxTransform OoxTransform {
+            get {
+                return _transform;
+            }
+        }
+
+        public OoxShape(OoxTransform transform) {
+            _transform = transform;
+           
+        }
+
+    }
+}
