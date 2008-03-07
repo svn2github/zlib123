8932f758dec62c5230c2957de5690cc84b53f172
tomek_mueller <tomek_mueller@24c45a59-3718-0410-950f-d1d872dd8675>
2008-03-07 12:21:18 +0000

    Localized Czech Add-in added
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@4416 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/Spreadsheet/Setup/OdfAddInForExcelSetup-cz/SetupPrepare.bat
@@ -0,0 +1,9 @@
+mkdir KB908002
+move extensibilityMSM.msi KB908002
+move lockbackRegKey.msi KB908002
+move office2003-kb907417sfxcab-ENU.exe KB908002
+mkdir PIA2003
+move o2003pia.msi PIA2003
+move o2003pia_eula.txt PIA2003
+
+start /W Setup.exe
