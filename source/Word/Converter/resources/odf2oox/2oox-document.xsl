svn2github/zlib123

commit af4ed1a94f3be2c43feb084682a9950bb3221bc4

Changed to position of the "TOC end" field. The "TOC end" will no longer converted to the last paragraph of the TOC; it will be converted to the first paragraph after the TOC.
git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@4890 24c45a59-3718-0410-950f-d1d872dd8675

source/Word/Converter/resources/odf2oox/2oox-document.xsl
@@ -213,6 +213,11 @@
       <xsl:when test="parent::text:index-body">
         <xsl:call-template name="InsertIndexItem"/>
       </xsl:when>
+      
+      <!-- this is the first paragraph after the TOC, insert the TOC end -->
+      <xsl:when test="preceding-sibling::text:table-of-content[1]">
+        <xsl:call-template name="InsertIndexFieldCodeEnd"/>
+      </xsl:when>
 
       <!-- ignore draw:frame/draw:text-box if it's embedded in another draw:frame/draw:text-box because word doesn't support it -->
       <xsl:when test="self::node()[ancestor::draw:text-box and descendant::draw:text-box]">

source/Word/Converter/resources/odf2oox/2oox-indexes.xsl
@@ -83,11 +83,26 @@
       </xsl:otherwise>
     </xsl:choose>
 
-    <!-- inserts field code end in last index element -->
+    <!-- inserts field code end in last index element
     <xsl:if test="(count(following-sibling::text:p) = 0) and parent::text:index-body">
       <xsl:call-template name="InsertIndexFieldCodeEnd"/>
     </xsl:if>
-
+    
+    makz: 
+    Inserting the TOC end in the last paragraph of the TOC produces a docx file that 
+    can be displayed by Word but it causes problems on a roundtrip.
+    The converter awaits the TOC end in the first paragraph AFTER the TOC!!!! 
+    This is the default behavior of Word.
+    
+    Thus the TOC end will only be inserted if there is NO following paragraph.
+    Else the paragraph conversion insetrs the TOC end.
+    -->
+    <xsl:if test="
+            count(following-sibling::text:p) = 0 and 
+            parent::text:index-body and 
+            not(../following::text:p or ../following::text:h)">
+      <xsl:call-template name="InsertIndexFieldCodeEnd"/>
+    </xsl:if>
   </xsl:template>
 
   <!-- end field -->

commit 6f1dad122a62324df3e4650dafbbdeb732b951d3

Fix for regressions introduced in r4890 (paragraphs were lost)
git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@4907 24c45a59-3718-0410-950f-d1d872dd8675

source/Word/Converter/resources/odf2oox/2oox-document.xsl
@@ -213,11 +213,6 @@
       <xsl:when test="parent::text:index-body">
         <xsl:call-template name="InsertIndexItem"/>
       </xsl:when>
-      
-      <!-- this is the first paragraph after the TOC, insert the TOC end -->
-      <xsl:when test="preceding-sibling::text:table-of-content[1]">
-        <xsl:call-template name="InsertIndexFieldCodeEnd"/>
-      </xsl:when>
 
       <!-- ignore draw:frame/draw:text-box if it's embedded in another draw:frame/draw:text-box because word doesn't support it -->
       <xsl:when test="self::node()[ancestor::draw:text-box and descendant::draw:text-box]">
@@ -268,6 +263,12 @@
       </xsl:when>
 
       <xsl:otherwise>
+
+        <!-- this is the first paragraph after the TOC, insert the TOC end -->
+        <xsl:if test="preceding-sibling::text:table-of-content[1]">
+          <xsl:call-template name="InsertIndexFieldCodeEnd"/>
+        </xsl:if>
+        
         <xsl:apply-templates mode="paragraph"/>
       </xsl:otherwise>
 