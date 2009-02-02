bf39f7cbde76a2be16077341d771bae0093cf72c
divo76 <divo76@24c45a59-3718-0410-950f-d1d872dd8675>
2009-02-02 17:41:59 +0000

    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@5076 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/AdditionalTools/ProcessHelper/WaitForProcess/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WaitForProcess")]
+[assembly: AssemblyDescription("A simply utility which makes the calling process wait until a specified process has exited.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DIaLOGIKa")]
+[assembly: AssemblyProduct("WaitForProcess")]
+[assembly: AssemblyCopyright("Copyright © DIaLOGIKa 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9b1ce99-8356-4faf-a25f-0846a4b775b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
