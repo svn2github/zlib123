59a738803a5de508298f6eede8398bb8ec240b06
pradeepgn <pradeepgn@24c45a59-3718-0410-950f-d1d872dd8675>
2009-02-19 12:46:51 +0000

    Added a file remotely
    git-svn-id: http://svn.code.sf.net/p/odf-converter/code/trunk@5132 24c45a59-3718-0410-950f-d1d872dd8675

---
+++ source/Common/OdfConverterLib/CSharpEval.cs
@@ -0,0 +1,104 @@
+﻿/* 
+ * Copyright (c) 2006, SONATA SOFTWARE LTD.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Clever Age nor the names of its contributors 
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.JScript;
+
+namespace OdfConverter.CSharpEval
+{
+    public class Evaluator
+    {
+        public static int EvalToInteger(string statement)
+        {
+            string s = EvalToString(statement);
+            return int.Parse(s.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        public static double EvalToDouble(string statement)
+        {
+            string s = EvalToString(statement);
+            if (s == "NaN")
+            {
+                return 0;
+            }
+            else
+            {
+                return double.Parse(s, System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static string EvalToString(string statement)
+        {
+            object o = EvalToObject(statement);
+            return o.ToString();
+        }
+
+        public static object EvalToObject(string statement)
+        {
+            return _evaluatorType.InvokeMember(
+                        "Eval",
+                        BindingFlags.InvokeMethod,
+                        null,
+                        _evaluator,
+                        new object[] { statement }
+                     );
+        }
+
+        static Evaluator()
+        {
+            CodeDomProvider compiler = CodeDomProvider.CreateProvider("JScript");
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateInMemory = true;
+
+            CompilerResults results;
+            results = compiler.CompileAssemblyFromSource(parameters, _jscriptSource);
+
+            Assembly assembly = results.CompiledAssembly;
+            _evaluatorType = assembly.GetType("Evaluator.Evaluator");
+
+            _evaluator = Activator.CreateInstance(_evaluatorType);
+        }
+
+        private static object _evaluator = null;
+        private static Type _evaluatorType = null;
+        private static readonly string _jscriptSource =
+
+            @"package Evaluator
+            {
+               class Evaluator
+               {
+                  public function Eval(expr : String) : String 
+                  { 
+                     return eval(expr); 
+                  }
+               }
+            }";
+    }
+}
